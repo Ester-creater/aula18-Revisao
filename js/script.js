bb848e24625c55187289f55e5cf9fd7aecdeb3a3
Helo Ester <helo.esteralmeida@gmail.com>
2024-11-25 21:55:56 -0300

    Aula18

---
+++ js/script.js
@@ -0,0 +1,67 @@
+function mostraAlerta(){
+    alert('Isso funciona mesmo!!')
+}
+
+function mostraAlerta1(msg){
+    alert('Boa Noite' + msg)
+}
+
+function soma(n1, n2){
+    n1=parseFloat(n1);
+    n2=parseFloat(n2);
+    soma=n1+n2;
+    return(soma);
+}
+
+function dobro() {
+    var valor1, result
+    valor1 = document.getElementById("idN");
+    result = parseFloat(valor1.value) 
+    +parseFloat(valor1.value);
+    document.getElementById("idResult"). 
+    innerHTML = result;
+}
+
+function calculadora(op) {
+    var valor1, valor2, result
+    valor1= document.getElementById("idN11");
+    valor2= document.getElementById("idN22");
+
+    if (op == "+")
+    {
+        result = parseFloat(valor1.value)
+        +parseFloat(valor2.value);
+        document.getElementById("idRespostas").
+        innerHTML = result;
+        
+    }
+
+    if (op == "-")
+    {
+   
+    result = parseFloat(valor1.value)
+        -parseFloat(valor2.value);
+    document.getElementById("idRespostas").
+    innerHTML = result;
+
+    }
+
+    if (op =="*")
+    {
+   
+    result = parseFloat(valor1.value)
+        *parseFloat(valor2.value);
+        document.getElementById("idRespostas").
+    innerHTML = result;
+
+    }
+    if (op =="/")
+    {
+   
+    result = parseFloat(valor1.value)
+        /parseFloat(valor2.value);
+        document.getElementById("idRespostas").
+    innerHTML = result;
+
+    }
+}
